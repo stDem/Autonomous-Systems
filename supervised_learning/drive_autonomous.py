f655d1855c31e0b5071c1b24ca5a16243ff8dfba
stDem <61629062+stDem@users.noreply.github.com>
2026-01-29 13:24:54 +0100

    Update drive_autonomous.py

--- supervised_learning/drive_autonomous.py
+++ supervised_learning/drive_autonomous.py
@@ -238,7 +238,7 @@ def main():
     # ---- init car ----
     car = NvidiaRacecar()
     car.steering_gain = 1.0
-    car.steering_offset = -0.18
+    car.steering_offset = 0.18
     car.throttle_gain = 1.0
     car.steering = 0.0
     car.throttle = 0.0
